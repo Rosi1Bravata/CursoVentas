dab3ac9fb8aa8e80f5ba9f1ba0956e3b1c349274
ROSITA BRAVATA DE LA CRUZ <ti_4222010410@utcam.edu.mx>
2024-03-01 17:23:51 -0600

    Buenas tardes profesor esta es mi actividad Alumna Rosita Bravata De La Cruz 5B

---
+++ app/src/main/java/com/example/cursoventas/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.cursoventas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var  firebaseAuth: FirebaseAuth
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+       val btningresar : Button = findViewById(R.id.btnIngresar)
+        val txtemail : TextView = findViewById(R.id.edtEmail)
+        val txtpass : TextView = findViewById(R.id.edtPassword)
+        val btnCrear_CuentaNueva : TextView = findViewById(R.id.btnCrearCuenta)
+
+        firebaseAuth= Firebase.auth
+        btningresar.setOnClickListener()
+        {
+             signIn(txtemail.text.toString(),txtpass.text.toString())
+        }
+        btnCrear_CuentaNueva.setOnClickListener()
+        {
+            val i = Intent( this,CreaCuentaActivity::class.java)
+            startActivity(i)
+
+        }
+    }
+    private fun  signIn(email: String, password: String)
+    {
+     firebaseAuth.signInWithEmailAndPassword(email, password).
+     addOnCompleteListener(this) { task ->
+         if (task.isSuccessful) {
+             val user = firebaseAuth.currentUser
+             Toast.makeText(baseContext,"Atenticacion Existosa",
+             Toast.LENGTH_SHORT).show()
+             // aqui vamos a ir a la segunda activity
+             val i = Intent(this, MainActivity2::class.java)
+             startActivity(i)
+         }
+         else
+         {
+             Toast.makeText(baseContext,"Error de Email y/o Contraseña", Toast.LENGTH_SHORT).show()
+         }
+     }
+    }
+}
